fcf28a6d7227ac96779351a5b815ea93fcd44c40
Andy Zimolzak (2018 mbp) <andyzimolzak@gmail.com>
2022-08-05 00:27:45 -0500

    parse numbers in grid now, working decent.

--- parse_wsjt.py
+++ parse_wsjt.py
@@ -1,17 +1,29 @@
-# import fileinput
+# usage is something like:
+# watch -n 14 python parse_wsjt.py
 
-def grid_matches(line):
-    message = line[48:]
+def extract_grid(line):
+    message = line[48:]  # assume column 48 is start of message
     words = message.split()
-    grid = words[-1]
-    return grid[0:2] == 'DN'
+    return words[-1]  # assume last word is grid
 
-def tail_once():
+def grid_matches(line, digraph_sought='DN'):
+    grid = extract_grid(line)
+    return grid[0:2] == digraph_sought
+
+def coords_match(line, xl, xh, yl, yh):
+    grid = extract_grid(line)
+    x = int(grid[2])
+    y = int(grid[3])
+    return (xl <= x <= xh) and (yl <= y <= yh)
+
+def tail_once(n_lines=100):
     with open('/Users/ajz/Library/Application Support/WSJT-X/ALL.TXT') as fh:
         all_lines = fh.readlines()
-        tail = all_lines[-100:]
+        tail = all_lines[(-1 * n_lines):]
         for line in tail:
-            if grid_matches(line):
+            if grid_matches(line, 'DN'):
                 print(line, end='')
+                if coords_match(line, 2, 7, 5, 8):  # 2~7 and 5~8 are montana
+                    print(' ' * 4, '^' * 60)
 
-tail_once()
+tail_once(100)
