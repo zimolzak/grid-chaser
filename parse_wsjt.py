74a23728517926423f89d90cb1f4b6d653bd144f
Andy Zimolzak (2018 mbp) <andyzimolzak@gmail.com>
2022-08-05 00:01:51 -0500

    print matching lines, suitable for WATCH.

--- parse_wsjt.py
+++ parse_wsjt.py
@@ -1,17 +1,17 @@
 # import fileinput
 
-# for line in fileinput.input():
-#     message = line[48:]
-#     words = message.split()
-#     grid = words[-1]
-#     if grid[0:2] == 'DN':
-#         print(line, end='')
+def grid_matches(line):
+    message = line[48:]
+    words = message.split()
+    grid = words[-1]
+    return grid[0:2] == 'DN'
 
 def tail_once():
     with open('/Users/ajz/Library/Application Support/WSJT-X/ALL.TXT') as fh:
         all_lines = fh.readlines()
-        tail = all_lines[-25:]
+        tail = all_lines[-100:]
         for line in tail:
-            print(line, end='')
+            if grid_matches(line):
+                print(line, end='')
 
 tail_once()
