b2bc8e196e0a27a462e46aa7c07bb62b282a7f02
Andy Zimolzak (2018 mbp) <andyzimolzak@gmail.com>
2022-08-04 23:47:20 -0500

    Reads file from disk instead of stdin

--- parse_wsjt.py
+++ parse_wsjt.py
@@ -1,8 +1,17 @@
-import fileinput
+# import fileinput
 
-for line in fileinput.input():
-    message = line[48:]
-    words = message.split()
-    grid = words[-1]
-    if grid[0:2] == 'DN':
-        print(line, end='')
+# for line in fileinput.input():
+#     message = line[48:]
+#     words = message.split()
+#     grid = words[-1]
+#     if grid[0:2] == 'DN':
+#         print(line, end='')
+
+def tail_once():
+    with open('/Users/ajz/Library/Application Support/WSJT-X/ALL.TXT') as fh:
+        all_lines = fh.readlines()
+        tail = all_lines[-25:]
+        for line in tail:
+            print(line, end='')
+
+tail_once()
